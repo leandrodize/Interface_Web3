82854193016c854eb2403bc38ce385d3f7e4ea73
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-12-07 18:08:30 -0500

    Local MAIN: add config web3

---
+++ src/config/web3/index.js
@@ -0,0 +1,12 @@
+import Web3 from "web3";
+import { InjectedConnector } from '@web3-react/injected-connector';
+
+const connector = new InjectedConnector({ supportedChainIds: [4] })
+//4 is network Rinkeby
+const getLibrary = (provider) => {
+    return new Web3(provider);
+}
+
+
+export {connector, getLibrary};
+
