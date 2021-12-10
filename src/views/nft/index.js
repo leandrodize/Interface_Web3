c45a8196cd757c530423c9d109170f77f59a3e9d
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-12-09 23:44:27 -0500

    Local MAIN: transfer NFTs from others accounts

--- src/views/nft/index.js
+++ src/views/nft/index.js
@@ -10,6 +10,7 @@ import {
     Tbody,
     Button,
     Tag,
+    useToast,
   } from "@chakra-ui/react";
   import { useWeb3React } from "@web3-react/core";
   import RequestAccess from "../../components/request-acces";
@@ -17,11 +18,56 @@ import {
   import { useTransHumansData } from "../../hooks/useTransHumansData";
   import { useParams } from "react-router-dom";
   import Loading from "../../components/loading";
+  import { useState } from "react";
   
   const NFT = () => {
-    const { active, account } = useWeb3React();
+    const { active, account, library } = useWeb3React();
     const { tokenId } = useParams();
-    const { loading, trans } = useTransHumansData(tokenId);
+    const { loading, trans, update } = useTransHumansData(tokenId);
+    const toast = useToast();
+    const [transfering, setTransfering] = useState(false);
+    const TransHumans = useTransHumansData();
+
+    const transfer = () => {
+      setTransfering(true);
+      const address = prompt("Ingresa la direccion");
+      const isAddress = library.utils.isAddress(address);
+
+      if (!isAddress) {
+        toast({
+          title: "Direccion invalida",
+          description: "Por favor ingresa una direccion valida",
+          status: "error",
+        });
+        setTransfering(false);
+      }else{
+        TransHumans.methods
+        .safeTransferFrom(trans.owner, address, trans.tokenId)
+        .send({
+          from: account,
+        })
+        .on('error', () => {
+          setTransfering(false);
+        })
+        .on('transactionHash', (txHash) =>{
+          toast({
+            title: "Transaccion Invalida",
+            description: txHash,
+            status: "info",
+          });
+        })
+        .on('receipt', () => {
+          setTransfering(false);
+          toast({
+            title: "Transaccion Confirmada",
+            description: `El NFT le pertenece a ${address}`,
+            status: "success",
+          });
+          update();
+        });
+      }
+
+    }
   
     if (!active) return <RequestAccess />;
   
@@ -42,7 +88,12 @@ import {
             name={trans.name}
             image={trans.image}
           />
-          <Button disabled={account !== trans.owner} colorScheme="blue">
+          <Button 
+            onClick={transfer} 
+            disabled={account !== trans.owner} 
+            colorScheme="blue"
+            isLoading={transfering}
+            >
             {account !== trans.owner ? "No eres el dueño" : "Transferir"}
           </Button>
         </Stack>
